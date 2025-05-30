5d8d60cbe3e8b0ce8e691c08c41473deaeb47bbe
jeffedwards <jeffrey.d.edwards@gmail.com>
2025-05-29 19:36:24 -0700

    added tailwind files manually.  May not need

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tells Tailwind to look in your src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

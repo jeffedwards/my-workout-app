c78ebc9aa07eebee90fcb47633e23e16d5958cf5
jeffedwards <jeffrey.d.edwards@gmail.com>
2025-05-29 20:46:41 -0700

    Manual update

--- src/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // This tells PostCSS to use the new Tailwind v4 plugin
-    'autoprefixer': {},       // This is standard and good practice
-  }
-}
